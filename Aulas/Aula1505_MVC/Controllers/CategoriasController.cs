714ff8fca5bdb3c9247a3e1d4f48db883c7ee571
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-15 21:40:10 -0300

    Criando view

--- Aulas/Aula1505_MVC/Controllers/CategoriasController.cs
+++ Aulas/Aula1505_MVC/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aula1505_MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,17 @@ namespace Aula1505_MVC.Controllers
         // GET: Categorias
         public ActionResult Index()
         {
+
+            List<Categoria> categorias = new List<Categoria>();
+
+            categorias.Add(new Categoria() { Nome = "Casa" });
+            categorias.Add(new Categoria() { Nome = "Carro" });
+            categorias.Add(new Categoria() { Nome = "Trabalho" });
+            categorias.Add(new Categoria() { Nome = "Família" });
+
+
+            ViewBag.MinhasCategorias = categorias;
+
             return View();
         }
     }
