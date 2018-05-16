40fe161ddfb6aaa6d169bc59e14d5bef0b097f06
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-15 21:28:27 -0300

    Adicionando CategoriasController

---
+++ Aulas/Aula1505_MVC/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula1505_MVC.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
