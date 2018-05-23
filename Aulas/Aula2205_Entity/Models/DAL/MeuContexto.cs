ebc5d5995b2dfd7d682da1c909101597a5660f15
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-22 21:19:37 -0300

    Criando MeuContexto

--- Aulas/Aula2205_Entity/Models/DAL/MeuContexto.cs
+++ Aulas/Aula2205_Entity/Models/DAL/MeuContexto.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace Aula2205_Entity.Models.DAL
 {
-    public class MeuContexto
+    public class MeuContexto : DbContext
     {
+        public MeuContexto() : base("strConn")
+        {
 
+        }
     }
 }
