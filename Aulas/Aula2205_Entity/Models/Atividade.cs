c913a17427a6144fe329c852e1cfafc76724cd4e
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-24 19:42:00 -0300

    Criando model Atividade

--- Aulas/Aula2205_Entity/Models/Atividade.cs
+++ Aulas/Aula2205_Entity/Models/Atividade.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Aula2205_Entity.Models
 {
     public class Atividade
     {
+        
         public int AtividadeID { get; set; }
 
+        [Required]
         public string Nome { get; set; }
 
         public string Descricao { get; set; }
