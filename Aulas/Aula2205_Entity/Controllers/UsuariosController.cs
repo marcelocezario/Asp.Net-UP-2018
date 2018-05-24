cfe2aa435f0aa861a5e51c921ba32e969640e7a9
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-24 20:04:01 -0300

    Criando UsuariosController

---
+++ Aulas/Aula2205_Entity/Controllers/UsuariosController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Aula2205_Entity.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
