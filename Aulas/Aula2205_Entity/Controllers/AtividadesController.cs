42039c101276e5c217df4e41d8a947b3b4421c52
Marcelo Cezário <marcelocezario@gmail.com>
2018-05-30 00:50:53 -0300

    Criando AtividadesController

---
+++ Aulas/Aula2205_Entity/Controllers/AtividadesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula2205_Entity.Controllers
+{
+    public class AtividadesController : Controller
+    {
+        // GET: Atividades
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
